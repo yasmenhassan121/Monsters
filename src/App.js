e70feb554b1f25c33591bcc3dc818529b6246590
yasmenhassan <yasmenah34@gmail.com>
2022-01-15 00:38:37 +0200

    app updates

--- src/App.js
+++ src/App.js
@@ -18,7 +18,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <CardList name='yasmena'>
+        <CardList name='yasmenaaa'>
           {this.state.monsters.map((monster) => (
             <h1 key={monster.id}> {monster.name} </h1>
           ))}
