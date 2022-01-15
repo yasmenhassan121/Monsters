d4cfb5d82df28f456ad16254a74e04c55b498e1a
yasmenhassan <yasmenah34@gmail.com>
2022-01-15 23:40:36 +0200

    add searchField component

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ class App extends Component {
     super();
     this.state = {
       monsters: [],
+      searchField:' '
     };
   }
   componentDidMount() {
@@ -18,6 +19,13 @@ class App extends Component {
   render() {
     return (
       <div className="App">
+        <input
+          type='search'
+          placeholder="search monster"
+          onChange={e => {
+            this.setState({ searchField: e.target.value }, () => console.log(this.state));
+          }}
+        />
         <CardList monsters={this.state.monsters}> </CardList>
       </div>
     );
